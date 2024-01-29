b3e80f8e9bd83f0862dbdebe77c227f4a4f2b73b
Arvind <arvind.ubarri@gmail.com>
2024-01-29 12:20:24 +0530

    new fixes

--- controller/productController.js
+++ controller/productController.js
@@ -9,11 +9,18 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 // Payment Gateway
+// var gateway = new braintree.BraintreeGateway({
+//   environment: braintree.Environment.Sandbox,
+//   merchantId: process.env.BRAINTREE_MERCHANT_ID,
+//   publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+//   privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+// });
+
 var gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+  merchantId: "826hhgdxn8j9cvyn",
+  publicKey: "dw4qznzb6nd8r73r",
+  privateKey: "e72fc2b23599118f1a43ee918d83a602",
 });
 
 export const createProductController = async (req, res) => {
